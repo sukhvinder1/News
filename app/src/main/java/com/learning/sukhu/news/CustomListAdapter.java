9ef72877eb72723a314144d0fa61261064bcd684
Sukhvinder <sukhvindersingh02@gmail.com>
2016-09-22 23:53:07 -0400

    Working sources listview with custome adaptor

---
+++ app/src/main/java/com/learning/sukhu/news/CustomListAdapter.java
@@ -0,0 +1,49 @@
+package com.learning.sukhu.news;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.learning.sukhu.news.Dtos.SourcesDto;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by quocnguyen on 03/08/2016.
+ */
+public class CustomListAdapter extends ArrayAdapter<SourcesDto> {
+
+    ArrayList<SourcesDto> sources;
+    Context context;
+    int resource;
+
+    public CustomListAdapter(Context context, int resource, ArrayList<SourcesDto> sources) {
+        super(context, resource, sources);
+        this.sources = sources;
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater) getContext()
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        convertView = layoutInflater.inflate(R.layout.channels_custom_list, null, true);
+
+        SourcesDto sources = getItem(position);
+
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageView);
+        Picasso.with(context).load(sources.getUrlSize()).into(imageView);
+
+        TextView txtName = (TextView) convertView.findViewById(R.id.sourcesList);
+        txtName.setText(sources.getName());
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/learning/sukhu/news/SelectChannelsActivity.java
+++ app/src/main/java/com/learning/sukhu/news/SelectChannelsActivity.java
@@ -3,11 +3,16 @@ package com.learning.sukhu.news;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.learning.sukhu.news.Dtos.SourcesDto;
 import com.learning.sukhu.news.Json.GetSourcesJsonData;
 import com.learning.sukhu.news.Transportation.SourcesDataBus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +22,8 @@ public class SelectChannelsActivity extends AppCompatActivity implements Sources
 
     private String Log_Tag = "Sukh_tag_SelectChannelsActivity";
     private GetSourcesJsonData getSourcesJsonData;
+    private ArrayList<SourcesDto> sourcesList;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,9 @@ public class SelectChannelsActivity extends AppCompatActivity implements Sources
     public void onStart(){
         super.onStart();
 
+        sourcesList = new ArrayList<>();
+        listView = (ListView)findViewById(R.id.sourcesListView);
+
         getSourcesJsonData = new GetSourcesJsonData("Hello", this);
         getSourcesJsonData.execute();
     }
@@ -34,5 +44,18 @@ public class SelectChannelsActivity extends AppCompatActivity implements Sources
     @Override
     public void processedData(List<SourcesDto> sources) {
         Log.v("Sukh_tag/******", sources.get(0).getName());
+
+        //creating sourceList of Sources
+        sourcesList.addAll(sources);
+
+        CustomListAdapter adapter = new CustomListAdapter(getApplicationContext(), R.layout.channels_custom_list, sourcesList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(), sourcesList.get(position).getName(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
